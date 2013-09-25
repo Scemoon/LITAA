d05f677ed1c59a6978d4b08c8da0def1169e7d46
Scemoon <mengsan8325150@gmail.com>
2013-09-25 22:25:52 +0800

    20130925-22:25

--- Web/ResultApp/admin.py
+++ Web/ResultApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from ResultApp.models import ResultsList
 class ResultsAdmin(admin.ModelAdmin):
-    fields = ['TestID', 'TestTool','TestTime']
+    pass
 admin.site.register(ResultsList, ResultsAdmin)
--- Web/ResultApp/models.py
+++ Web/ResultApp/models.py
@@ -2,9 +2,17 @@ from django.db import models
 
 # Create your models here.
 class ResultsList(models.Model):
-    tools = ('lmbench','unixbench','stream')
-    ResultsID=models.AutoField(primary_key=True,unique=True)
+    tools = (('LH','lmbench'),('UH','unixbench'),('SM','stream'))
+    archtype = (('x86','X86'),('x64','X64'))
+    ResultsID=models.AutoField(primary_key=True)
     TestTool=models.CharField(max_length=2,choices=tools)
+    Arch=models.CharField(max_length=3,choices=archtype)
+    OS=models.CharField(max_length=20)
+    Tester=models.CharField(max_length=20)
     TestTime=models.DateField("TestDate")
+    
+class Lmbench(models.Model):
+    pass
+    
     def __unicode__(self):
         return self.TestTool
