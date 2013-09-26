ac1d50b04a06168d6749cfa71d9b9b18fc02575d
scemoon <mengsan8325150@gmail.com>
2013-09-26 17:33:39 +0800

    20130926-17:33

--- Web/ResultApp/admin.py
+++ Web/ResultApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from ResultApp.models import ResultsList
 class ResultsAdmin(admin.ModelAdmin):
-    pass
+    fields=['TestTool','Testarch','OS','Tester','TestTime']
 admin.site.register(ResultsList, ResultsAdmin)
--- Web/ResultApp/models.py
+++ Web/ResultApp/models.py
@@ -5,14 +5,17 @@ class ResultsList(models.Model):
     tools = (('LH','lmbench'),('UH','unixbench'),('SM','stream'))
     archtype = (('x86','X86'),('x64','X64'))
     ResultsID=models.AutoField(primary_key=True)
-    TestTool=models.CharField(max_length=2,choices=tools)
-    Arch=models.CharField(max_length=3,choices=archtype)
+    TestTool=models.CharField(max_length=10,choices=tools)
+    Testarch=models.CharField(max_length=10,choices=archtype)
     OS=models.CharField(max_length=20)
-    Tester=models.CharField(max_length=20)
-    TestTime=models.DateField("TestDate")
+    Tester=models.CharField(max_length=20,blank=True)
+    TestTime=models.DateField()
+    def __unicode__(self):
+        return self.Testarch
     
 class Lmbench(models.Model):
-    pass
+    resultID=models.ForeignKey(ResultsList)
+     
     
     def __unicode__(self):
         return self.TestTool
